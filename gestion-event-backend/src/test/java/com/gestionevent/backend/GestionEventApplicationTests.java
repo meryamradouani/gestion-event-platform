54304e8a489ceaf5f375f5864274786e77ba9ab8
amalik <salma.amalik@uit.ac.ma>
2025-12-16 17:10:47 +0100

    Premier commit

---
+++ gestion-event-backend/src/test/java/com/gestionevent/backend/GestionEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionevent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
