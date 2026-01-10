ead8ee276989b6ca36d62b414513d9a607e21660
meryamm <maryame0.125@gmail.com>
2026-01-10 23:48:50 +0100

    test pipeline

--- registration-service/src/test/java/com/gestionevent/registrationservice/controller/RegistrationControllerIntegrationTest.java
+++ registration-service/src/test/java/com/gestionevent/registrationservice/controller/RegistrationControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.gestionevent.registrationservice.controller;
 
+public class RegistrationControllerIntegrationTest {
+
     @org.junit.jupiter.api.Test
     void contextLoads() {
         // Test de base pour vérifier le chargement du contexte
--- registration-service/src/test/java/com/gestionevent/registrationservice/service/KafkaEventTest.java
+++ registration-service/src/test/java/com/gestionevent/registrationservice/service/KafkaEventTest.java
@@ -1,5 +1,7 @@
 package com.gestionevent.registrationservice.service;
 
+public class KafkaEventTest {
+
     @org.junit.jupiter.api.Test
     void contextLoads() {
         // Test de base pour satisfaire SonarCloud
